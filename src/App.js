453da0678c2f9a81214d39baddd8c98b47af7400
coconutkim <achaeyeon18@gmail.com>
2024-12-05 12:19:51 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+  return <TodoTemplate>let's create todo app</TodoTemplate>
+  //div 대신에 todotemplate을 사용한다
+}
+
+export default App;
